f2f5548f4bd42e94846290a5b493b371a52cec91
andrerpena <andrerpena@gmail.com>
2016-11-30 07:18:01 -0200

    Added multiple tests for the TextHelper

--- src/TextHelper.js
+++ src/TextHelper.js
@@ -9,3 +9,42 @@
 export function  insertText(text, insertionText, position) {
     return [text.slice(0, position), insertionText, text.slice(position)].join('');
 }
+
+/**
+ * Gets the word surrounding the given position. Word delimiters are spaces and line-breaks
+ * 
+ * @export
+ * @param {any} text
+ * @param {any} position
+ */
+export function getSurroundingWord(text, position) {
+
+    if(!text) throw Error('Argument \'text\' should be truthy');
+    
+    let isWordDelimiter = c => c == ' ' || c.charCodeAt(0) == 10;
+
+    let leftIndex = 0; // leftIndex is initialized to 0 because if position is 0, it won't even enter the iteration
+    let rightIndex = text.length; // rightIndex is initialized to text.length because if position is equal to text.length it won't even enter the interation
+    
+    // iterate to the left
+    for(var i = position; i -1 > -1; i--) {
+        if(isWordDelimiter(text[i - 1])) {
+            leftIndex = i;
+            break;
+        }
+    }
+
+    // iterate to the right
+    for(var i = position; i < text.length ; i++) {
+        if(isWordDelimiter(text[i])) {
+            rightIndex = i;
+            break;
+        }
+    }
+
+    return {
+        word: text.slice(leftIndex, rightIndex),
+        leftIndex: leftIndex,
+        rightIndex: rightIndex
+    };
+}
--- test/TextHelperSpec.js
+++ test/TextHelperSpec.js
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import { insertText } from '../src/TextHelper';
+import { insertText, getSurroundingWord } from '../src/TextHelper';
 
 describe("TextHelperSpec.js", function () {
     describe('insertText', function () {
@@ -20,5 +20,88 @@ describe("TextHelperSpec.js", function () {
             var text = insertText('bob went to', ' school', 20);
             assert.strictEqual(text, 'bob went to school');
         });
-    })
+    });
+
+    describe('getSurroundingWord', function () {
+        // tests without line breaks
+        it('Basic usage', function () {
+            var result = getSurroundingWord('bob went to school', 5);
+            assert.strictEqual(result.word, 'went');
+            assert.strictEqual(result.leftIndex, 4);
+            assert.strictEqual(result.rightIndex, 8);
+        });
+
+        it('Word edge to the right', function () {
+            var result = getSurroundingWord('bob went to school', 8);
+            assert.strictEqual(result.word, 'went');
+            assert.strictEqual(result.leftIndex, 4);
+            assert.strictEqual(result.rightIndex, 8);
+        });
+
+        it('Word edge to the left', function () {
+            var result = getSurroundingWord('bob went to school', 4);
+            assert.strictEqual(result.word, 'went');
+            assert.strictEqual(result.leftIndex, 4);
+            assert.strictEqual(result.rightIndex, 8);
+        });
+
+        it('text beginning', function () {
+            var result = getSurroundingWord('bob went to school', 0);
+            assert.strictEqual(result.word, 'bob');
+            assert.strictEqual(result.leftIndex, 0);
+            assert.strictEqual(result.rightIndex, 3);
+        });
+
+        it('text ending', function () {
+            var result = getSurroundingWord('bob went to school', 18);
+            assert.strictEqual(result.word, 'school');
+            assert.strictEqual(result.leftIndex, 12);
+            assert.strictEqual(result.rightIndex, 18);
+        });
+
+        // tests with line breaks
+        it('Basic usage with line breaks', function () {
+            var result = getSurroundingWord('bob\nwent\nto school', 5);
+            assert.strictEqual(result.word, 'went');
+            assert.strictEqual(result.leftIndex, 4);
+            assert.strictEqual(result.rightIndex, 8);
+        });
+
+        it('text ending with line break', function () {
+            var result = getSurroundingWord('bob went to school\n', 18);
+            assert.strictEqual(result.word, 'school');
+            assert.strictEqual(result.leftIndex, 12);
+            assert.strictEqual(result.rightIndex, 18);
+        });
+
+        it('text beginning with line break', function () {
+            var result = getSurroundingWord('\nbob went to school', 1);
+            assert.strictEqual(result.word, 'bob');
+            assert.strictEqual(result.leftIndex, 1);
+            assert.strictEqual(result.rightIndex, 4);
+        });
+
+        // edge cases
+        it('within spaces', function() {
+            var result = getSurroundingWord('   ', 1);
+            assert.strictEqual(result.word, '');
+            assert.strictEqual(result.leftIndex, 1);
+            assert.strictEqual(result.rightIndex, 1);
+        });
+
+        it('within spaces 2', function() {
+            var result = getSurroundingWord('   ', 2);
+            assert.strictEqual(result.word, '');
+            assert.strictEqual(result.leftIndex, 2);
+            assert.strictEqual(result.rightIndex, 2);
+        });
+
+        it('within line-breaks', function() {
+            var result = getSurroundingWord('\n\n\n', 1);
+            assert.strictEqual(result.word, '');
+            assert.strictEqual(result.leftIndex, 1);
+            assert.strictEqual(result.rightIndex, 1);
+        });
+    });
+
 });
