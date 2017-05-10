bb18854bbff350e08ebab2db1607052de9548bc5
André Pena <andrerpena@gmail.com>
2017-05-10 14:49:23 -0300

    Lint-fixing the ReactMdeTextHelper.js file

--- demo/server.js
+++ demo/server.js
@@ -1,17 +1,16 @@
 import fs from 'fs';
-import React from 'react';
 import express from 'express';
-import webpackConfig from '../webpack.config.demo.dev';
 import colors from 'colors';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpack from 'webpack';
+import webpackConfig from '../webpack.config.demo.dev';
 import packageJson from '../package.json';
 
 
 const webpackCompiler = webpack(webpackConfig);
 
-require.extensions['.html'] = function (module, filename) {
+require.extensions['.html'] = (module, filename) => {
     module.exports = fs.readFileSync(filename, 'utf8');
 };
 
@@ -19,7 +18,9 @@ const app = express();
 
 app.use(webpackMiddleware(webpackCompiler));
 app.use(webpackHotMiddleware(webpackCompiler));
+/*eslint-disable*/
 app.use((req, res) => res.status(200).send(require('./index.html')));
+/*eslint-enable*/
 
 app.listen(4000, '0.0.0.0', () => {
     console.log(colors.green(`${packageJson.name} at http://localhost:4000/`));
--- src/ReactMdeTextHelper.js
+++ src/ReactMdeTextHelper.js
@@ -53,11 +53,64 @@ export function insertAfter(text, insertionText, selection) {
     const textInsertion = insertText(text, insertionText, selection[1]);
     const newText = textInsertion.newText;
     const insertionLength = textInsertion.insertionLength;
-    let newSelection;
-
-    newSelection = [selection[0], selection[1] + insertionLength];
+    const newSelection = [selection[0], selection[1] + insertionLength];
 
     return { newText, newSelection };
+}
+
+/**
+ *  Gets the number of breaks needed so that there will be an empty line between the previous text
+ */
+export function getBreaksNeededForEmptyLineBefore(text = '', startPosition) {
+    if (startPosition === 0) return 0;
+
+    // rules:
+    // - If we're in the first line, no breaks are needed
+    // - Otherwise there must be 2 breaks before the previous character. Depending on how many breaks exist already, we
+    //      may need to insert 0, 1 or 2 breaks
+
+    let neededBreaks = 2;
+    let isInFirstLine = true;
+    for (let i = startPosition - 1; i >= 0 && (neededBreaks >= 0); i--) {
+        switch (text.charCodeAt(i)) {
+            case 32: continue;
+            case 10: {
+                neededBreaks--;
+                isInFirstLine = false;
+                break;
+            }
+            default: return neededBreaks;
+        }
+    }
+    return isInFirstLine ? 0 : neededBreaks;
+}
+
+/**
+ *  Gets the number of breaks needed so that there will be an empty line after the next text
+ */
+export function getBreaksNeededForEmptyLineAfter(text, startPosition) {
+    if (!text) throw Error('Argument \'text\' should be truthy');
+    if (startPosition === text.length - 1) return 0;
+
+    // rules:
+    // - If we're in the first line, no breaks are needed
+    // - Otherwise there must be 2 breaks before the previous character. Depending on how many breaks exist already, we
+    //      may need to insert 0, 1 or 2 breaks
+
+    let neededBreaks = 2;
+    let isInLastLine = true;
+    for (let i = startPosition; i < text.length && (neededBreaks >= 0); i++) {
+        switch (text.charCodeAt(i)) {
+            case 32: continue;
+            case 10: {
+                neededBreaks--;
+                isInLastLine = false;
+                break;
+            }
+            default: return neededBreaks;
+        }
+    }
+    return isInLastLine ? 0 : neededBreaks;
 }
 
 /**
@@ -124,10 +177,11 @@ export function insertBeforeEachLine(text, insertion, selection) {
             insertionLength += insertion.length;
             return insertion + item;
         } else if (typeof insertion === 'function') {
-            const _insertion = insertion(item, index);
-            insertionLength += _insertion.length;
+            const insertionResult = insertion(item, index);
+            insertionLength += insertionResult.length;
             return insertion(item, index) + item;
         }
+        throw Error('insertion is expected to be either a string or a function');
     }).join('\n');
 
     const newText = text.slice(0, selection[0]) + modifiedText + text.slice(selection[1]);
@@ -135,22 +189,6 @@ export function insertBeforeEachLine(text, insertion, selection) {
 }
 
 // MISC
-
-
-/**
- * Returns the selection of the current work if selection[0] is equal to selection[1] and carret is inside a word
- *
- * @export
- * @param {any} text
- * @param {any} selection
- */
-export function selectCurrentWorkIfCarretIsInsideOne(text, selection) {
-    if (text && text.length && selection[0] == selection[1]) {
-        // the user is pointing to a word
-        return getSurroundingWord(text, selection[0]).position;
-    }
-    return selection;
-}
 
 /**
  * Gets the word surrounding the given position. Word delimiters are spaces and line-breaks
@@ -162,13 +200,13 @@ export function selectCurrentWorkIfCarretIsInsideOne(text, selection) {
 export function getSurroundingWord(text, position) {
     if (!text) throw Error('Argument \'text\' should be truthy');
 
-    const isWordDelimiter = c => c == ' ' || c.charCodeAt(0) == 10;
+    const isWordDelimiter = c => c === ' ' || c.charCodeAt(0) === 10;
 
     let leftIndex = 0; // leftIndex is initialized to 0 because if position is 0, it won't even enter the iteration
     let rightIndex = text.length; // rightIndex is initialized to text.length because if position is equal to text.length it won't even enter the interation
 
     // iterate to the left
-    for (var i = position; i - 1 > -1; i--) {
+    for (let i = position; i - 1 > -1; i--) {
         if (isWordDelimiter(text[i - 1])) {
             leftIndex = i;
             break;
@@ -176,7 +214,7 @@ export function getSurroundingWord(text, position) {
     }
 
     // iterate to the right
-    for (var i = position; i < text.length; i++) {
+    for (let i = position; i < text.length; i++) {
         if (isWordDelimiter(text[i])) {
             rightIndex = i;
             break;
@@ -190,56 +228,16 @@ export function getSurroundingWord(text, position) {
 }
 
 /**
- *  Gets the number of breaks needed so that there will be an empty line between the previous text
+ * Returns the selection of the current work if selection[0] is equal to selection[1] and carret is inside a word
+ *
+ * @export
+ * @param {any} text
+ * @param {any} selection
  */
-export function getBreaksNeededForEmptyLineBefore(text = '', startPosition) {
-    if (startPosition == 0) return 0;
-
-    // rules:
-    // - If we're in the first line, no breaks are needed
-    // - Otherwise there must be 2 breaks before the previous character. Depending on how many breaks exist already, we
-    //      may need to insert 0, 1 or 2 breaks
-
-    let neededBreaks = 2;
-    let isInFirstLine = true;
-    for (let i = startPosition - 1; i >= 0 && (neededBreaks >= 0); i--) {
-        switch (text.charCodeAt(i)) {
-            case 32: continue;
-            case 10: {
-                neededBreaks--;
-                isInFirstLine = false;
-                break;
-            }
-            default: return neededBreaks;
-        }
+export function selectCurrentWorkIfCarretIsInsideOne(text, selection) {
+    if (text && text.length && selection[0] === selection[1]) {
+        // the user is pointing to a word
+        return getSurroundingWord(text, selection[0]).position;
     }
-    return isInFirstLine ? 0 : neededBreaks;
-}
-
-/**
- *  Gets the number of breaks needed so that there will be an empty line after the next text
- */
-export function getBreaksNeededForEmptyLineAfter(text, startPosition) {
-    if (!text) throw Error('Argument \'text\' should be truthy');
-    if (startPosition == text.length - 1) return 0;
-
-    // rules:
-    // - If we're in the first line, no breaks are needed
-    // - Otherwise there must be 2 breaks before the previous character. Depending on how many breaks exist already, we
-    //      may need to insert 0, 1 or 2 breaks
-
-    let neededBreaks = 2;
-    let isInLastLine = true;
-    for (let i = startPosition; i < text.length && (neededBreaks >= 0); i++) {
-        switch (text.charCodeAt(i)) {
-            case 32: continue;
-            case 10: {
-                neededBreaks--;
-                isInLastLine = false;
-                break;
-            }
-            default: return neededBreaks;
-        }
-    }
-    return isInLastLine ? 0 : neededBreaks;
+    return selection;
 }
