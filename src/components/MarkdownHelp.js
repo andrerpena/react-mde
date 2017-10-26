62da1a0a077d486b40ee3685ad9a19de0703e46c
Jonas Faber <jonas@graphcms.com>
2017-10-26 14:49:25 +0200

    Move to seperate prop-types package.
    See https://github.com/facebook/prop-types#prop-types

--- src/components/MarkdownHelp.js
+++ src/components/MarkdownHelp.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const MarkdownHelp = ({ helpText, markdownReferenceUrl }) => (
     <a className="markdown-help" href={markdownReferenceUrl} target="_blank" rel="noopener noreferrer">
