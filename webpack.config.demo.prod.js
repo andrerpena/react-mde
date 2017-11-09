d973aa6d3811b0b4249009b3fa9255dd0c753a9e
André Pena <andrerpena@gmail.com>
2017-11-09 08:08:23 -0200

    Fixing the demo (again)

--- webpack.config.demo.prod.js
+++ webpack.config.demo.prod.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         filename: 'bundle-prod.js',
         path: path.resolve(__dirname, 'docs'),
-        publicPath: ''
+        publicPath: '/react-mde/'
     },
 
     resolve: {
