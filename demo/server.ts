3af4e796ee430eede2db07f756e42614cc500373
André Pena <andrerpena@gmail.com>
2017-12-09 09:09:22 -0200

    Making the "start" dev script to open the browser automatically

--- demo/server.ts
+++ demo/server.ts
@@ -5,9 +5,11 @@ import * as webpackMiddleware from 'webpack-dev-middleware';
 import * as webpackHotMiddleware from 'webpack-hot-middleware';
 import * as webpack from 'webpack';
 import * as webpackConfig from '../webpack.config.demo.dev';
+import * as opn from 'opn';
 const packageJson  = require('../package.json');
 
 const webpackCompiler = webpack(webpackConfig as any);
+const port = 4000;
 
 require.extensions['.html'] = (module, filename) => {
     module.exports = fs.readFileSync(filename, 'utf8');
@@ -19,6 +21,8 @@ app.use(webpackMiddleware(webpackCompiler));
 app.use(webpackHotMiddleware(webpackCompiler));
 app.use((req, res) => res.status(200).send(require('./index.html')));
 
-app.listen(4000, '0.0.0.0', () => {
-    console.log(colors.green(`${packageJson.name} at http://localhost:4000/`));
+app.listen(port, '0.0.0.0', () => {
+    const demoUrl = `http://localhost:${port}/`;
+    console.log(colors.green(`${packageJson.name} at ${demoUrl}`));
+    opn(demoUrl);
 });
