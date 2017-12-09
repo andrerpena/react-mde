0c3460fc98ad0cab331199e0f084759f998b7bdc
André Pena <andrerpena@gmail.com>
2017-12-09 15:34:07 -0200

    Bump to 2.3.0

--- docs/bundle-prod.js
+++ docs/bundle-prod.js
@@ -1428,14 +1428,18 @@ var ReactMdeTextArea = /** @class */ (function (_super) {
          */
         _this.handleValueChange = function (e) {
             var onChange = _this.props.onChange;
-            onChange({ text: e.currentTarget.value, selection: null });
+            onChange({ text: e.currentTarget.value });
         };
         return _this;
     }
     ReactMdeTextArea.prototype.componentDidUpdate = function () {
-        var selection = this.props.value.selection;
+        var _a = this.props.value, selection = _a.selection, textareaScrollTop = _a.textareaScrollTop;
         if (selection) {
             ReactMdeSelectionHelper_1.setSelection(this.textArea, selection.start, selection.end);
+        }
+        if (textareaScrollTop !== null && textareaScrollTop !== undefined) {
+            // This is necessary because otherwise, when the value is reset, the scroll will jump to the end
+            this.textArea.scrollTop = textareaScrollTop;
         }
     };
     ReactMdeTextArea.prototype.render = function () {
@@ -18933,7 +18937,7 @@ exports.makeQuoteCommand = {
         textInsertion = ReactMdeTextHelper_1.insertBreaksBeforeSoThatThereIsAnEmptyLineBefore(text, selection);
         text = textInsertion.newText;
         selection = textInsertion.newSelection;
-        textInsertion = ReactMdeTextHelper_1.insertBefore(text, "> ", selection);
+        textInsertion = ReactMdeTextHelper_1.insertBefore(text, "> ", selection, false);
         text = textInsertion.newText;
         selection = textInsertion.newSelection;
         textInsertion = ReactMdeTextHelper_1.insertBreaksAfterSoThatThereIsAnEmptyLineAfter(text, selection);
@@ -23517,6 +23521,41 @@ var __assign = (this && this.__assign) || Object.assign || function(t) {
     }
     return t;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = __webpack_require__(0);
 var ReactMdeSelectionHelper_1 = __webpack_require__(8);
@@ -23539,16 +23578,26 @@ var ReactMde = /** @class */ (function (_super) {
          * Executes a command
          * @memberOf ReactMde
          */
-        _this.handleCommand = function (command) {
-            var _a = _this.props, text = _a.value.text, onChange = _a.onChange;
-            var newValue = command.execute(text, ReactMdeSelectionHelper_1.getSelection(_this.textArea));
-            if (newValue instanceof Promise) {
-                newValue.then(function (v) { return onChange(v); });
-            }
-            else {
-                onChange(newValue);
-            }
-        };
+        _this.handleCommand = function (command) { return __awaiter(_this, void 0, void 0, function () {
+            var _a, text, onChange, newValue;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        _a = this.props, text = _a.value.text, onChange = _a.onChange;
+                        newValue = command.execute(text, ReactMdeSelectionHelper_1.getSelection(this.textArea));
+                        if (!(newValue instanceof Promise)) return [3 /*break*/, 2];
+                        return [4 /*yield*/, newValue];
+                    case 1:
+                        newValue = _b.sent();
+                        _b.label = 2;
+                    case 2:
+                        // This is necessary because otherwise, when the value is reset, the scroll will jump to the end
+                        newValue.textareaScrollTop = this.textArea.scrollTop;
+                        onChange(newValue);
+                        return [2 /*return*/];
+                }
+            });
+        }); };
         return _this;
     }
     /**
