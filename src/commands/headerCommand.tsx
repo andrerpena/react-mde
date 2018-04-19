7740d231c5d66151313763d8bb7578160886d4ff
Nikita Golovin <golovin.nik@gmail.com>
2018-04-19 08:35:16 +0300

    adding 4th header

--- src/commands/headerCommand.tsx
+++ src/commands/headerCommand.tsx
@@ -8,24 +8,31 @@ export const headerCommand: Command = {
     buttonProps: { "aria-label": "Add header" },
     children: [
         {
-            buttonContent: <p className="header-1">Header</p>,
+            buttonContent: <p className="header-1">Header 1</p>,
             execute: (getMarkdownState, setMarkdownState) => {
                 const {text, selection} = getMarkdownState();
                 setMarkdownState(makeHeader({text, selection}, "# "));
             },
         },
         {
-            buttonContent: <p className="header-2">Header</p>,
+            buttonContent: <p className="header-2">Header 2</p>,
             execute: (getMarkdownState, setMarkdownState) => {
                 const {text, selection} = getMarkdownState();
                 setMarkdownState(makeHeader({text, selection}, "## "));
             },
         },
         {
-            buttonContent: <p className="header-3">Header</p>,
+            buttonContent: <p className="header-3">Header 3</p>,
             execute: (getMarkdownState, setMarkdownState) => {
                 const {text, selection} = getMarkdownState();
                 setMarkdownState(makeHeader({text, selection}, "### "));
+            },
+        },
+        {
+            buttonContent: <p className="header-4">Header 4</p>,
+            execute: (getMarkdownState, setMarkdownState) => {
+                const {text, selection} = getMarkdownState();
+                setMarkdownState(makeHeader({text, selection}, "#### "));
             },
         },
     ],
