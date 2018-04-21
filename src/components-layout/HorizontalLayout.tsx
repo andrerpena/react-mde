81d9b81c5d5887dbd9b9129b6514bf9aaf406545
Nikita Golovin <golovin.nik@gmail.com>
2018-04-21 12:55:55 +0300

    code/preview switchers in toolbar: horizontal layout

--- src/components-layout/HorizontalLayout.tsx
+++ src/components-layout/HorizontalLayout.tsx
@@ -3,8 +3,13 @@ import {Command} from "../types";
 import {ReactMde} from "../ReactMde";
 import {MdePreview, MdeEditor, MdeToolbar} from "../components";
 import {LayoutProps} from "../types/LayoutProps";
+import {TAB_CODE, TAB_PREVIEW} from "./TabbedLayout";
 
 export class HorizontalLayout extends React.Component<LayoutProps, {}> {
+    state = {
+        showCode: true,
+        showPreview: true,
+    };
 
     editorRef: MdeEditor;
     previewRef: MdePreview;
@@ -23,31 +28,70 @@ export class HorizontalLayout extends React.Component<LayoutProps, {}> {
         onCommand(command);
     }
 
+    handleShowCode = () => {
+        if (!this.state.showCode || this.state.showPreview)
+            this.setState({showCode: !this.state.showCode});
+    };
+
+    handleShowPreview = () => {
+        if (!this.state.showPreview || this.state.showCode)
+            this.setState({showPreview: !this.state.showPreview});
+    };
+
     /**
      * Renders react-mde
      * @returns
      * @memberOf ReactMde
      */
     render() {
-
         const { commands, mdeEditorState } = this.props;
+
+        let styleTabCode = "mde-tab";
+        let styleTabPreview = "mde-tab";
+        let stylePreview = null;
+        if (this.state.showCode)
+            styleTabCode += " mde-tab-activated";
+        else
+            stylePreview = "mde-preview-only";
+        if (this.state.showPreview)
+            styleTabPreview += " mde-tab-activated";
 
         return (
             <div className="react-mde-horizontal-layout">
                 <MdeToolbar
                     commands={commands}
                     onCommand={this.handleCommand}
-                />
-                <div className="react-mde-content">
-                    <MdeEditor
-                        editorRef={(c) => this.editorRef = c}
-                        onChange={this.handleMdeStateChange}
-                        editorState={mdeEditorState}
-                    />
-                    <MdePreview
-                        previewRef={(c) => this.previewRef = c}
-                        html={mdeEditorState ? mdeEditorState.html : ""}
-                    />
+                >
+                    <div className="mde-tabs">
+                        <button
+                            className={styleTabCode}
+                            onClick={this.handleShowCode}
+                        >
+                            Code
+                        </button>
+                        <button
+                            className={styleTabPreview}
+                            onClick={this.handleShowPreview}
+                        >
+                            Preview
+                        </button>
+                    </div>
+                </MdeToolbar>
+                <div className="mde-content">
+                    {this.state.showCode &&
+                        <MdeEditor
+                            editorRef={(c) => this.editorRef = c}
+                            onChange={this.handleMdeStateChange}
+                            editorState={mdeEditorState}
+                        />
+                    }
+                    {this.state.showPreview &&
+                        <MdePreview
+                            className={stylePreview}
+                            previewRef={(c) => this.previewRef = c}
+                            html={mdeEditorState ? mdeEditorState.html : ""}
+                        />
+                    }
                 </div>
             </div>
         );
