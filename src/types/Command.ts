f3bac6ef3a5c1fa6aa86401a55ecdb5a56b11a33
andre <andrerpena@gmail.com>
2018-06-26 20:19:47 +0200

    Bump to 5.5.3

--- src/types/Command.ts
+++ src/types/Command.ts
@@ -2,12 +2,9 @@ import * as React from "react";
 import {MarkdownState} from "./MarkdownState";
 import {EditorState} from "draft-js";
 
-type Execute = (EditorState, data?) => EditorState;
-type ExecutePromise = (EditorState, data?) => Promise<EditorState>;
-
 export interface Command {
     buttonContent: React.ReactNode;
     buttonProps?: any;
     children?: Command[];
-    execute?: Execute | ExecutePromise;
+    execute?: (EditorState, data?) => EditorState | Promise<EditorState>;
 }
