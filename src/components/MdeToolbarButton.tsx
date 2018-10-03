d19efd1508b1b07f04b267e0aefee31e33d23f67
andrerpena <andrerpena@gmail.com>
2018-10-03 15:23:23 +0200

    Making the buttons to be type="button" to prevent form submittion. Fixes #138 #140

--- src/components/MdeToolbarButton.tsx
+++ src/components/MdeToolbarButton.tsx
@@ -1,24 +1,25 @@
 import * as React from "react";
 
 export interface MdeToolbarButtonProps {
-    buttonComponentClass?: React.ComponentClass | string;
-    buttonProps: any;
-    buttonContent: React.ReactNode;
-    onClick: React.MouseEventHandler<any>;
-    readOnly: boolean;
+  buttonComponentClass?: React.ComponentClass | string;
+  buttonProps: any;
+  buttonContent: React.ReactNode;
+  onClick: React.MouseEventHandler<any>;
+  readOnly: boolean;
 }
 
 export const MdeToolbarButton: React.SFC<MdeToolbarButtonProps> = (props) => {
-    const {buttonComponentClass, buttonContent, buttonProps, onClick, readOnly} = props;
-    const finalButtonComponent = buttonComponentClass || "button";
-    return (
-        <li className="mde-header-item">
-            {React.createElement(finalButtonComponent, {
-                ...buttonProps, ...{
-                    onClick,
-                    disabled: readOnly,
-                },
-            }, buttonContent)}
-        </li>
-    );
+  const { buttonComponentClass, buttonContent, buttonProps, onClick, readOnly } = props;
+  const finalButtonComponent = buttonComponentClass || "button";
+  return (
+    <li className="mde-header-item">
+      {React.createElement(finalButtonComponent, {
+        ...buttonProps, ...{
+          onClick,
+          disabled: readOnly,
+          type: "button"
+        }
+      }, buttonContent)}
+    </li>
+  );
 };
