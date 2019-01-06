a818292dd54516f618b9a90e64a5bf7e800b05c6
andrerpena <andrerpena@gmail.com>
2019-01-06 17:23:21 +0100

    Add italic and strikethrough commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,8 +2,8 @@ import { boldCommand } from "./boldCommand";
 // import { codeCommand } from "./codeCommand";
 // import { headerCommand } from "./headerCommand";
 // import { imageCommand } from "./imageCommand";
-// import { italicCommand } from "./italicCommand";
-// import { strikethroughCommand } from "./strikethroughCommand";
+import { italicCommand } from "./italicCommand";
+import { strikeThroughCommand } from "./strikethroughCommand";
 // import { linkCommand } from "./linkCommand";
 // import { orderedListCommand } from "./orderedListCommand";
 // import { quoteCommand } from "./quoteCommand";
@@ -12,13 +12,13 @@ import { boldCommand } from "./boldCommand";
 import { CommandGroup } from "../types";
 
 // const getDefaultCommands: () => CommandGroup[] = () => [
-//   { commands: [headerCommand, boldCommand, italicCommand, strikethroughCommand] },
+//   { commands: [headerCommand, strikeThroughCommand, italicCommand, strikethroughCommand] },
 //   { commands: [linkCommand, quoteCommand, codeCommand, imageCommand] },
 //   { commands: [unorderedListCommand, orderedListCommand, checkListCommand] }
 // ];
 
 const getDefaultCommands: () => CommandGroup[] = () => [
-  { commands: [ boldCommand ] }
+  { commands: [boldCommand, italicCommand, strikeThroughCommand] }
 ];
 
 export {
--- src/commands/strikethroughCommand.tsx
+++ src/commands/strikethroughCommand.tsx
@@ -1,16 +1,23 @@
-// import * as React from "react";
-// import { Command } from "../types";
-// import { insertBeforeAndAfter } from "../util/MarkdownUtil";
-// import { buildNewDraftState, getMarkdownStateFromDraftState } from "../util/DraftUtil";
-//
-// export const strikethroughCommand: Command = {
-//   buttonContentBuilder: ({ iconProvider }) => iconProvider("strikethrough"),
-//
-//   buttonProps: { "aria-label": "Add strikethrough text" },
-//
-//   execute: (state) => {
-//     let mdState = getMarkdownStateFromDraftState(state);
-//     mdState = insertBeforeAndAfter(mdState, "~~");
-//     return buildNewDraftState(state, mdState);
-//   }
-// };
+import * as React from "react";
+import { Command } from "../types";
+import { TextApi, TextState } from "../types/CommandOptions";
+import { selectWord } from "../util/MarkdownUtil";
+
+export const strikeThroughCommand: Command = {
+  name: "strikethrough",
+  buttonContentBuilder: ({ iconProvider }) => iconProvider("strikethrough"),
+  buttonProps: { "aria-label": "Add strikethrough text" },
+  execute: (state0: TextState, api: TextApi) => {
+    // Adjust the selection to encompass the whole word if the caret is inside one
+    const newSelectionRange = selectWord({ text: state0.text, selection: state0.selection });
+    const state1 = api.setSelectionRange(newSelectionRange);
+    // Replaces the current selection with the bold mark up
+    const state2 = api.replaceSelection(`_${state1.selectedText}_`);
+    // Adjust the selection to not contain the **
+    api.setSelectionRange({
+      start: state2.selection.end - 1 - state1.selectedText.length,
+      end: state2.selection.end - 1
+    });
+  },
+  keyCommand: "strikethrough",
+};
