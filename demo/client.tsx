26f099e882d9216bfaf666f22f8c433fb72039b0
andrerpena <andrerpena@gmail.com>
2019-11-11 07:38:58 +0100

    Remove draft.css from the demo

--- demo/client.tsx
+++ demo/client.tsx
@@ -3,7 +3,6 @@ import { render } from "react-dom";
 import { App } from "./App";
 
 import "../node_modules/normalize.css/normalize.css";
-import "../node_modules/draft-js/dist/Draft.css";
 import "../src/styles/react-mde-all.scss";
 import "./styles/demo.scss";
 import "./styles/variables.scss";
