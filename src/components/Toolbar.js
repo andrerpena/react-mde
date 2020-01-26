d6be8ba07ff4e8bbf5ca5bcd97b5420c1354bcab
Matt <carlotta.matt@gmail.com>
2020-01-25 22:15:58 -0800

    Adjust toolbar and body styling

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -27,6 +27,39 @@ export class Toolbar extends React.Component {
 
     return (
       <div className={classNames("mde-header", classes)}>
+        {commands.map((commandGroup, i) => (
+          <React.Fragment key={i}>
+            <ToolbarButtonGroup>
+              {commandGroup.commands.map(c => {
+                if (c.children) {
+                  return (
+                    <ToolbarDropdown
+                      key={c.name}
+                      buttonProps={c.buttonProps}
+                      getIcon={getIcon}
+                      buttonContent={c.icon ? c.icon(getIcon) : getIcon(c.name)}
+                      commands={c.children}
+                      onCommand={cmd => onCommand(cmd)}
+                      readOnly={readOnly}
+                    />
+                  );
+                }
+                return (
+                  <ToolbarButton
+                    key={c.name}
+                    name={c.name}
+                    buttonContent={c.icon ? c.icon(getIcon) : getIcon(c.name)}
+                    buttonProps={c.buttonProps}
+                    onClick={() => onCommand(c)}
+                    readOnly={readOnly}
+                    buttonComponentClass={c.buttonComponentClass}
+                  />
+                );
+              })}
+            </ToolbarButtonGroup>
+            <i className="separator">|</i>
+          </React.Fragment>
+        ))}
         <div className="mde-tabs">
           {!disablePreview ? (
             this.props.tab === "preview" ? (
@@ -49,37 +82,8 @@ export class Toolbar extends React.Component {
               </button>
             )
           ) : null}
+          {!disablePreview && <i className="separator">|</i>}
         </div>
-        {commands.map((commandGroup, i) => (
-          <ToolbarButtonGroup key={i}>
-            {commandGroup.commands.map((c, j) => {
-              if (c.children) {
-                return (
-                  <ToolbarDropdown
-                    key={j}
-                    buttonProps={c.buttonProps}
-                    getIcon={getIcon}
-                    buttonContent={c.icon ? c.icon(getIcon) : getIcon(c.name)}
-                    commands={c.children}
-                    onCommand={cmd => onCommand(cmd)}
-                    readOnly={readOnly}
-                  />
-                );
-              }
-              return (
-                <ToolbarButton
-                  key={j}
-                  name={c.name}
-                  buttonContent={c.icon ? c.icon(getIcon) : getIcon(c.name)}
-                  buttonProps={c.buttonProps}
-                  onClick={() => onCommand(c)}
-                  readOnly={readOnly}
-                  buttonComponentClass={c.buttonComponentClass}
-                />
-              );
-            })}
-          </ToolbarButtonGroup>
-        ))}
       </div>
     );
   }
