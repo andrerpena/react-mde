675de69c366856fd5441be1d566087024672e7a5
andrerpena@gmail.com <andrerpena@gmail.com>
2020-02-07 23:16:58 +0100

    Upgrade gulp and demo

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,59 @@
-const gulp = require('gulp');
-const rename = require('gulp-rename');
-const ts = require('gulp-typescript');
-const sass = require('gulp-sass');
-const merge = require('merge2');
-const tsProject = ts.createProject('./tsconfig.build.json');
-const webpackStream = require('webpack-stream');
+const gulp = require("gulp");
+const rename = require("gulp-rename");
+const ts = require("gulp-typescript");
+const sass = require("gulp-sass");
+const merge = require("merge2");
+const tsProject = ts.createProject("./tsconfig.build.json");
+const webpackStream = require("webpack-stream");
 
-gulp.task('build_styles', function () {
-    return gulp.src('./src/styles/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./lib/styles/css'));
-});
+function buildStyles() {
+  return gulp
+    .src("./src/styles/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./lib/styles/css"));
+}
 
 // library
-gulp.task('copy_styles', function () {
-    return gulp.src('./src/styles/**/*.scss')
-        .pipe(gulp.dest('./lib/styles/scss'));
-});
+function copyStyles() {
+  return gulp
+    .src("./src/styles/**/*.scss")
+    .pipe(gulp.dest("./lib/styles/scss"));
+}
 
-gulp.task('build-lib', ['copy_styles', 'build_styles'], function () {
-    const tsResult = tsProject.src()
-        .pipe(tsProject({
-            declaration: true
-        }));
-    return merge([
-        tsResult.dts.pipe(gulp.dest('lib/definitions')),
-        tsResult.js.pipe(gulp.dest('lib/js'))
-    ]);
-});
+// depends on copyStyles and buildStyles
+function buildLib() {
+  const tsResult = tsProject.src().pipe(
+    tsProject({
+      declaration: true
+    })
+  );
+  return merge([
+    tsResult.dts.pipe(gulp.dest("lib/definitions")),
+    tsResult.js.pipe(gulp.dest("lib/js"))
+  ]);
+}
 
 // demo
 // removes the output configuration from the webpack.config.js file, otherwise it doesn't work.
 
-gulp.task('copy-index', function () {
-    return gulp.src('./demo/index.prod.html')
-        .pipe(rename('index.html'))
-        .pipe(gulp.dest('./docs'));
-});
+function copyIndex() {
+  return gulp
+    .src("./demo/index.prod.html")
+    .pipe(rename("index.html"))
+    .pipe(gulp.dest("./docs"));
+}
 
-gulp.task('build-demo', ['copy-index'], function () {
-    return gulp.src('demo/client.ts')
-        .pipe(webpackStream(require('./webpack.config.demo.prod.js'), require("webpack")))
-        .pipe(gulp.dest('docs/'))
-});
+// depends on copyIndex
+function buildDemo() {
+  return gulp
+    .src("demo/client.tsx")
+    .pipe(
+      webpackStream(
+        require("./webpack.config.demo.prod.js"),
+        require("webpack")
+      )
+    )
+    .pipe(gulp.dest("docs/"));
+}
 
-// all
-gulp.task('build', ['build-demo', 'build-lib']);
+exports.build = gulp.series(buildStyles, copyStyles, copyIndex, buildLib, buildDemo);
