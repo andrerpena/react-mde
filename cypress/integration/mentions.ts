9d99dc435ff370da9424aa7646d9b037228f8c5e
andrerpena@gmail.com <andrerpena@gmail.com>
2020-04-29 21:37:57 +0100

    Add more UI tests

--- cypress/integration/mentions.ts
+++ cypress/integration/mentions.ts
@@ -10,6 +10,30 @@ context("Suggestions", () => {
       .type("{selectall}{backspace}")
       .type("@");
     cy.get(".mde-suggestions").should("exist");
+    cy.get(".mde-suggestions")
+      .find("li")
+      .should("have.length", 4);
+  });
+  it("'@' + 'an' should display 2 elements ", () => {
+    cy.get(".mde-suggestions").should("not.exist");
+    cy.get(".mde-text")
+      .type("{selectall}{backspace}")
+      .type("@an");
+    cy.get(".mde-suggestions").should("exist");
+    cy.get(".mde-suggestions")
+      .find("li")
+      .should("have.length", 2);
+  });
+  it("'@' + 'an' + 2x backspace should display 4 elements", () => {
+    cy.get(".mde-suggestions").should("not.exist");
+    cy.get(".mde-text")
+      .type("{selectall}{backspace}")
+      .type("@an")
+      .type("{backspace}{backspace}");
+    cy.get(".mde-suggestions").should("exist");
+    cy.get(".mde-suggestions")
+      .find("li")
+      .should("have.length", 4);
   });
   it("'@ + Enter' should select the first suggestion", () => {
     cy.get(".mde-text")
