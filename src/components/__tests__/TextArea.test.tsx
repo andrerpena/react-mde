914d53424a0157eddafb0012683b81dbc6007971
Javier Castro <javier.alejandro.castro@gmail.com>
2020-08-16 16:56:32 -0300

    TextArea.test.tsx: add missing onPaste & onDrop

--- src/components/__tests__/TextArea.test.tsx
+++ src/components/__tests__/TextArea.test.tsx
@@ -8,9 +8,16 @@ describe("<TextArea />", () => {
   it("loads and displays value", () => {
     let value = "# awesome title";
     const onChange = jest.fn();
+    const onPaste = jest.fn();
+    const onDrop = jest.fn();
 
     const { getByText } = render(
-      <TextArea value={value} onChange={onChange} />
+      <TextArea
+        value={value}
+        onChange={onChange}
+        onPaste={onPaste}
+        onDrop={onDrop}
+      />
     );
 
     expect(getByText(value));
