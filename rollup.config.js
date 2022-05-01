97257fc1f5f1d87787ef4bbb77247bfd82f7ace2
André Pena <andrerpena@gmail.com>
2022-05-01 23:46:22 +0100

    Remove unused files

--- rollup.config.js
+++ rollup.config.js
@@ -5,24 +5,24 @@ import typescript from "@rollup/plugin-typescript";
 const packageJson = require("./package.json");
 
 export default [
-    {
-        input: "src/index.ts",
-        output: [
-            {
-                file: packageJson.main,
-                format: "cjs",
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: "esm",
-                sourcemap: true,
-            },
-        ],
-        plugins: [
-            resolve(),
-            commonjs(),
-            typescript({ tsconfig: "./tsconfig.json" }),
-        ],
-    }
+  {
+    input: "src/index.ts",
+    output: [
+      // {
+      //     file: packageJson.main,
+      //     format: "cjs",
+      //     sourcemap: true,
+      // },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" })
+    ]
+  }
 ];
