748e2fa848dee7305f8399f8709af80f70ddd3f6
webbrother <komarov.vitaliy@gmail.com>
2022-11-29 12:32:27 +0400

    prettier.config.js added

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 120,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+};
